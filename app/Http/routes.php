9b20caed80ce42127396919f284fcbc5f6ef88d1
GevinHartanto <gevinhartanto@gmail.com>
2017-09-23 15:38:27 +0700

    Inserting Data

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Staff;
+use App\Photo;
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -13,4 +16,10 @@
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/create', function(){
+	$staff = Staff::find(1);
+	
+	$staff->photos()->create(['path'=>'example.jpg']);
 });
